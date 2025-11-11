3d653943034cbfbed4abeba1eabaabaf9e171f9e
TMorais9 <tiagosamorais2004@gmail.com>
2025-11-11 13:04:30 +0000

    FIx

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const {
   DB_PORT = 3306,
   DB_USER = 'root',
-  DB_PASSWORD = 'Mcmadias04',
-  DB_NAME = 'SoundCircle',
+  DB_PASSWORD = '2004Titofa1972',
+  DB_NAME = 'soundcircle',
 } = process.env;
 
 const connectionOptions = {
