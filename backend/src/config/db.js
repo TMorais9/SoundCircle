6177c80ba2885180cc0ea71513dba82fc2222fff
migueldias30 <150258425+migueldias30@users.noreply.github.com>
2025-11-30 16:16:27 +0000

    .

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const {
   DB_PORT = 3306,
   DB_USER = 'root',
-  DB_PASSWORD = 'Benfica1904',
-  DB_NAME = 'soundCircle',
+  DB_PASSWORD = 'Mcmadias04',
+  DB_NAME = 'SoundCircle',
 } = process.env;
 
 const connectionOptions = {
